1514cc3cffa9c0e2cf9d79f8123bc87f6dddbadd
MiyeongEom <aldjd020420@naver.com>
2022-10-18 22:02:34 +0900

    좌우 이동 구현

--- 2DGP GAME_Project/play_state.py
+++ 2DGP GAME_Project/play_state.py
@@ -7,28 +7,75 @@ import logo_state
 GAME_X = 1300
 GAME_Y = 600
 
-class Stage_ONE():
+class Stage_ONE:
     def __init__(self):
         self.stage1 = load_image('stage1_background.png')
 
     def draw(self):
-        self.stage1.draw(650, 300)
+        self.stage1.draw(GAME_X//2, GAME_Y//2)
 
-class Hero():
-    def __int__(self):
-        self.x, self.y = 0, 90
-        self.dir = 0
-        self.dir2 = 0
-        self.stand = load_image('MC_Idle.png')
-        self.walking = load_image('MC_Walking.png')
-        self.Run = load_image('MC_Run.png')
+class Hero:
+    frame = 0
+    ground = True
+    dir = 0
+    dir2 = 1
+    plus_move = 1
+    action = 0
+
+    def __init__(self):
+        self.x = 100
+        self.y = 90
+        self.stand_image = load_image('MC_Idle.png')
+        self.walking_image = load_image('MC_Walking.PNG')
+        # 592 102
 
     def update(self):
+        self.left = self.x - 30
+        self.right = self.x + 30
+        self.top = self.y - 30
+        self.bottom = self.y + 30
+        self.frame = (self.frame + 1) % 6
 
+        if self.dir == 1:
+            self.x += 20
+        elif self.dir == -1:
+            self.x -= 20
+
+        if self.dir != 0 and self.plus_move < 15:
+            self.plus_move += 1
+            if self.plus_move > 15:
+                self.plus_move = 15
+
+        elif self.dir == 0 and self.plus_move > 0:
+            self.plus_move -= 2
+            if self.plus_move < 0:
+                self.plus_move = 0
+
+        if self.dir != 0 and self.dir != self.dir2:
+            self.plus_move = 0
+
+        if self.x > 1260 and self.dir != -1:
+            self.x = 1260
+
+        elif self.x < 30 and self.dir != 1:
+            self.x = 30
 
     def draw(self):
+      # self.stand_image.clip_draw(int(self.frame) * 100, 0, 100, 100, self.x, self.y, 100, 100)
+      # delay(0.15)
 
-        pass
+        if self.dir == 1:  # 오른쪽
+            self.walking_image.clip_draw(int(self.frame) * 100, 0, 100, 100, self.x, self.y, 100, 100)
+
+        elif self.dir == -1:  # 왼쪽
+            self.walking_image.clip_composite_draw(int(self.frame) * 100, 0, 100, 100, 0, 'h', self.x, self.y, 100, 100)
+
+        elif self.dir == 0 and self.dir2 == 1:
+            self.stand_image.clip_draw(int(self.frame) * 100, 0, 100, 100, self.x, self.y, 100, 100)
+
+        elif self.dir == 0 and self.dir2 == -1:
+            self.stand_image.clip_composite_draw(int(self.frame) * 100, 0, 100, 100, 0, 'h', self.x, self.y, 100, 100)
+        delay(0.15)
 
 def handle_events():
     events = get_events()
@@ -36,32 +83,58 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                game_framework.change_state(title_state)
+            match event.key:
+                case pico2d.SDLK_ESCAPE:
+                    game_framework.change_state(title_state)
+                case pico2d.SDLK_RIGHT:
+                    main_hero.plus_move = 0
+                    main_hero.dir2 = 1
+                    main_hero.dir += 1
+
+                case pico2d.SDLK_LEFT:
+                    main_hero.plus_move = 0
+                    main_hero.dir2 = -1
+                    main_hero.dir -= 1
+
+                case pico2d.SDLK_a:
+                    main_hero.action = 1
 
 
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:  # 오른쪽
+                main_hero.dir -= 1
+            elif event.key == SDLK_LEFT:  # 왼쪽
+                main_hero.dir += 1
+
+main_hero = None
 stage1 = None
 running = None
 
 def enter():
-    global stage1, running
+    global stage1, running, main_hero
     stage1 = Stage_ONE()
+    main_hero = Hero()
     running = True
 
 # 게임 종료 - 객체 소멸
 def exit():
-    global stage1
+    global stage1, main_hero
     del stage1
+    del main_hero
 
 #게임 객체 업데이트 - 게임 로직
 def update():
+    main_hero.update()
     pass
 
 def draw():
-    # 개암 월드 렌더림
     clear_canvas()
-    stage1.draw()
+    draw_world()
     update_canvas()
+
+def draw_world():
+    stage1.draw()
+    main_hero.draw()
 
 def pause():
     pass
@@ -72,7 +145,7 @@ def resume():
 def test_self():
     import sys
     this_module = sys.modules['__main__']
-    pico2d.open_canvas()
+    pico2d.open_canvas(GAME_X, GAME_Y)
     game_framework.run(this_module)
     pico2d.close_canvas()
 
